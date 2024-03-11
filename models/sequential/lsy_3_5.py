b71b93acd05fc7a636b8130776406e39eda1b28d
asaander719 <21122587r@connect.polyu.hk>
2024-03-11 21:14:38 +0800

    3/11/2024/21:14

--- models/sequential/lsy_3_5.py
+++ models/sequential/lsy_3_5.py
@@ -30,7 +30,13 @@ class LSY(BaseModel):
         self.dropout_rate = configs['model']['dropout_rate']
         self.emb_layer = TransformerEmbedding(
             self.item_num + 2, self.hidden_size, self.max_len)
+        # self.layer_configs = [[64, 2], [256, 4]]
+        # self.transformer_layers = MultiScaleTransformer(self.layer_configs, feed_forward_size=self.hidden_size * 4, dropout_rate=self.dropout_rate)
+            # self.hidden_size, self.n_heads, self.hidden_size * 4, self.dropout_rate, scales=configs['model']["scales"])
+        # self.transformer_layers = MultiScaleTransformerLayer(
+        #     self.hidden_size, self.n_heads, self.hidden_size * 4, self.dropout_rate, scale_factors=configs['model']["scales"])
 
+        # self.buy_type = dataset.field2token_id["item_type_list"]['0']
         self.emb_size = configs['model']['embedding_size']
         self.mask_token = self.item_num + 1
         self.transformer_layers_1 = nn.ModuleList([TransformerLayer(
@@ -39,20 +45,42 @@ class LSY(BaseModel):
             self.emb_size, self.n_heads, self.emb_size * 2, self.dropout_rate) for _ in range(self.n_layers)])
         self.transformer_layers_3 = nn.ModuleList([TransformerLayer(
             self.emb_size, self.n_heads * 2, self.emb_size * 8, self.dropout_rate) for _ in range(self.n_layers)])
+        # self.mask_item_length = int(self.mask_ratio * self.max_seq_length)
+        # self.type_embedding = nn.Embedding(6, self.hidden_size, padding_idx=0)
+        # self.item_embedding = nn.Embedding(self.item_num +1, self.hidden_size, padding_idx=0)  # mask token add 1
+        # self.position_embedding = nn.Embedding(self.max_seq_length+1, self.hidden_size)  # add mask_token at the last
 
-        self.emb_nn = nn.Sequential(nn.Linear(self.hidden_size, self.hidden_size),nn.Sigmoid())#, nn.Dropout(configs['model']['hidden_dropout_prob'])) 
-        self.A_N = nn.Parameter(torch.Tensor(self.max_len, self.max_len))
-        nn.init.xavier_uniform_(self.A_N.data)
-        # self.reg_lambda = 0.001
-
-        self.att_aggre = configs['model']['att_aggre']
-        self.li_aggre_w = configs['tune']['att_aggre_w']
-
-        self.A_I = nn.Parameter(torch.Tensor(self.item_num + 2, self.item_num + 2))
-        nn.init.xavier_uniform_(self.A_I.data)
+        # if self.enable_ms:
+        #     self.trm_encoder = TransformerEncoder(
+        #         n_layers=self.n_layers,
+        #         num_heads=self.n_heads,
+        #         hidden_size=self.hidden_size,
+        #         inner_size=self.inner_size,
+        #         hidden_dropout_prob=self.hidden_dropout_prob,
+        #         attn_dropout_prob=self.attn_dropout_prob,
+        #         hidden_act=self.hidden_act,
+        #         layer_norm_eps=self.layer_norm_eps,
+        #         multiscale=True,
+        #         scales=configs['model']["scales"]
+        #     )
+        # else:
+        #     self.trm_encoder = TransformerEncoder(
+        #         n_layers=self.n_layers,
+        #         num_heads=self.n_heads,
+        #         hidden_size=self.hidden_size,
+        #         inner_size=self.inner_size,
+        #         hidden_dropout_prob=self.hidden_dropout_prob,
+        #         attn_dropout_prob=self.attn_dropout_prob,
+        #         hidden_act=self.hidden_act,
+        #         layer_norm_eps=self.layer_norm_eps,
+        #         multiscale=False
+        #     )
 
         self.hgnn_layer = HGNN(self.hidden_size)
+        # self.LayerNorm = nn.LayerNorm(self.hidden_size) #, eps=self.layer_norm_eps)
+        # self.dropout = nn.Dropout(self.hidden_dropout_prob)
 
+        # self.hg_type_embedding = nn.Embedding(6, self.hidden_size, padding_idx=0)
         self.metric_w1 = nn.Parameter(torch.Tensor(1, self.hidden_size))
         self.metric_w2 = nn.Parameter(torch.Tensor(1, self.hidden_size))
         self.gating_weight = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
@@ -70,6 +98,22 @@ class LSY(BaseModel):
         nn.init.normal_(self.metric_w1, std=0.02)
         nn.init.normal_(self.metric_w2, std=0.02)
 
+        # if self.dataset == "retail_beh":
+        #     self.sw_before = 10
+        #     self.sw_follow = 6
+        # elif self.dataset == "ijcai_beh":
+        #     self.sw_before = 30
+        #     self.sw_follow = 18
+        # elif self.dataset == "tmall_beh":
+        #     self.sw_before = 20
+        #     self.sw_follow = 12
+        # elif self.dataset == "ml-20m":
+        #     self.sw_before = 4
+        #     self.sw_follow = 2
+        # elif self.dataset == "beauty":
+        #     self.sw_before = 4
+        #     self.sw_follow = 2
+
         self.hypergraphs = dict()
 
         self.loss_func = nn.CrossEntropyLoss(ignore_index=0)
@@ -84,10 +128,6 @@ class LSY(BaseModel):
             module.weight.data.fill_(1.0)
         if isinstance(module, nn.Linear) and module.bias is not None:
             module.bias.data.zero_()
-
-    def reg_loss(self):
-        # L2 regularization on the A_n matrix
-        return torch.norm(self.A_N, p=2)
 
     def _transform_train_seq(self, batch_seqs, batch_last_items):
         device = batch_seqs.device
@@ -129,63 +169,76 @@ class LSY(BaseModel):
         return seqs[:, -self.max_len:]
 
     def forward(self, item_seq):
-        mask = (item_seq > 0).unsqueeze(1).repeat(1, item_seq.size(1), 1).unsqueeze(1)
+        mask = (item_seq > 0).unsqueeze(1).repeat(
+            1, item_seq.size(1), 1).unsqueeze(1)
         x = self.emb_layer(item_seq) #torch.Size([512, 50, 64])  
-        # x = self.emb_nn(x) #torch.Size([512, 50, 64])  # emb. low-dimentional mapping
-        if self.global_hg:
-            A_K = []
-            for batch_idx in range(item_seq.shape[0]):
-                seq = item_seq[batch_idx]
-                # Ensure that seq contains valid indices
-                if (seq < 0).any() or (seq >= self.A_I.size(0)).any():
-                    raise ValueError("Invalid indices in seq.")
-                A_I_seq = self.A_I.index_select(0, seq).index_select(1, seq) # 50,50
-                assert not torch.isnan(A_I_seq).any()
-                A_K_seq = 0.5 * ((A_I_seq + self.A_N) + (A_I_seq + self.A_N).t())
-                A_K.append(A_K_seq.unsqueeze(0))  # Add a batch dimension
+        x_raw = x * torch.sigmoid(x.matmul(self.gating_weight)+self.gating_bias) # torch.Size([128, 200, 64]) #func. 12?
+        # b, l, l #passed through a gating mechanism involving a sigmoid function
+        x_m = torch.stack((self.metric_w1*x_raw, self.metric_w2*x_raw)).mean(0) # torch.Size([128, 200, 64])
+        item_sim = self.sim(x_m, x_m)
+        item_sim[item_sim < 0] = 0.01 # torch.Size([128, 200, 200])
 
-            A_K = torch.cat(A_K, dim=0) #torch.Size([512, 50, 2500])
-        else: 
-            A_K = self.A_N.unsqueeze(0).expand(x.size(0), -1, -1)
-            
-        x_k = torch.matmul(A_K, x)
+        Gs = self.build_Gs_unique(item_seq, item_sim, self.hglen)
+
+        batch_size = item_seq.shape[0]
+        # print(item_seq.size())torch.Size([512, 201])
+        seq_len = item_seq.shape[1]
+        n_objs = torch.count_nonzero(item_seq, dim=1)
+        indexed_embs = list() # for each non-zero object in the sequence.
+        for batch_idx in range(batch_size):
+            n_obj = n_objs[batch_idx] # l', dim
+            indexed_embs.append(x_raw[batch_idx][:n_obj])
+        indexed_embs = torch.cat(indexed_embs, dim=0)
+        hgnn_embs = self.hgnn_layer(indexed_embs, Gs) #torch.Size([696, 64]) torch.Size([631, 64])
+        
+        hgnn_take_start = 0
+        hgnn_embs_padded = []
+        for batch_idx in range(batch_size):
+            n_obj = n_objs[batch_idx]
+            embs = hgnn_embs[hgnn_take_start:hgnn_take_start+n_obj]
+            hgnn_take_start += n_obj # l', dim || padding emb -> l, dim
+            padding = torch.zeros((seq_len-n_obj, embs.shape[-1])).to(item_seq.device)
+            embs = torch.cat((embs, padding), dim=0)
+            # pos = (item_seq[batch_idx]==self.mask_token).nonzero(as_tuple=True)[0][0]
+            # sliding_window_start = pos-self.sw_before if pos-self.sw_before>-1 else 0
+            # embs[pos] = torch.mean(embs[sliding_window_start:pos], dim=0)
+            hgnn_embs_padded.append(embs)
+        # b, l, dim
+        hgnn_embs = torch.stack(hgnn_embs_padded, dim=0)
         for transformer in self.transformer_layers_1:
             x_1 = transformer(x, mask)
-
-        if self.att_aggre: 
-            mixed_x = torch.stack((x_1, x_k), dim=0)
-            weights = (torch.matmul(mixed_x, self.attn_weights.unsqueeze(0).unsqueeze(0))*self.attn).sum(-1)
-            score = F.softmax(weights, dim=0).unsqueeze(-1)
-            mixed_x = (mixed_x*score).sum(0)
-        else:
-            mixed_x = self.li_aggre_w * x_1 + (1-self.li_aggre_w) * x_k
         # for transformer in self.transformer_layers_2:
         #     x_2 = transformer(hgnn_embs, mask)
         # for transformer in self.transformer_layers_3:
         #     x_3 = transformer(hgnn_embs, mask)
-        return self.emb_nn(x_1 + x_k) + x_1# + x_2 + x_3
+        return x_1, hgnn_embs # + x_2 + x_3
 
     def cal_loss(self, batch_data):
         batch_user, batch_seqs, batch_last_items = batch_data
         masked_seqs, masked_items = self._transform_train_seq(
             batch_seqs, batch_last_items.unsqueeze(1))
         # B, T, E
-        logits_t = self.forward(masked_seqs) # [b, l]
+        logits_t, logits_g = self.forward(masked_seqs) # [b, l]
         # loss_contrstive = torch.mean((logits_t - logits_g)**2)
 
         logits = self.out_fc(logits_t) # [b, l, n+1]
         # B, T, E -> B*T, E
         logits = logits.view(-1, logits.size(-1)) # [b*l, n+1]
         loss = self.loss_func(logits, masked_items.reshape(-1)) 
-        # reg_loss = self.reg_loss()
-        total_loss = loss #+ self.reg_lambda * reg_loss
-        loss_dict = {'rec_loss': total_loss.item()}#, "reg_loss": reg_loss.item()}
+
+        logits_g = self.out_fc(logits_g) # [b, l, n+1]
+        # B, T, E -> B*T, E
+        logits_g = logits_g.view(-1, logits_g.size(-1)) # [b*l, n+1]
+        loss_g = self.loss_func(logits_g, masked_items.reshape(-1)) 
+
+        total_loss = loss * 0.8 + 0.2 * loss_g      
+        loss_dict = {'rec_loss': total_loss.item(), 'loss_g': loss_g.item()}
         return total_loss, loss_dict
 
     def full_predict(self, batch_data):
         batch_user, batch_seqs, _ = batch_data
         masked_seqs = self._transform_test_seq(batch_seqs)
-        scores = self.forward(masked_seqs)
+        scores, scores_hg = self.forward(masked_seqs)
         scores = self.out_fc(scores)
         scores = scores[:, -1, :]
         return scores
